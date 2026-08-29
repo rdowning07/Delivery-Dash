6c9d602c3da993e187a2a12d6d0a8a8a787a5e84
robd <redowning@gmail.com>
2026-08-28 23:34:23 -0700

    First MonoBehavior

---
+++ Assets/Driver.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.Rotate(0, 0, 45);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
