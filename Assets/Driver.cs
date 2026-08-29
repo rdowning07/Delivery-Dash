b1bfd35c062d63900916e4cfbb44b311dcdbbf13
robd <redowning@gmail.com>
2026-08-28 23:51:45 -0700

    Variables - adding steerSpeed

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -2,16 +2,17 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    float steerSpeed = 0.5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.Rotate(0, 0, 45);
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        // transform.Rotate(0, 0, 0.02f);
-        transform.Translate(0, 0.01f, 0);
+        transform.Rotate(0, 0, steerSpeed);
+        transform.Translate(0, 0.05f, 0);
     }
 }
