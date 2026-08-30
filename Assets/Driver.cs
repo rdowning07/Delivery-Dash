edb5ee38f5c6f23c7e8d29f1098d2e16ab79fa9b
robd <redowning@gmail.com>
2026-08-30 14:54:27 -0700

    added wasd movement

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 0.1f;
@@ -13,6 +13,22 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Keyboard.current.wKey.isPressed)
+        {
+            Debug.Log("We are pushing forward");
+        }
+        if(Keyboard.current.aKey.isPressed)
+        {
+            Debug.Log("We are pushing left");
+        }
+        if(Keyboard.current.sKey.isPressed)
+        {
+            Debug.Log("We are pushing backward");
+        }
+        if(Keyboard.current.dKey.isPressed)
+        {
+            Debug.Log("We are pushing right");
+        }
         transform.Rotate(0, 0, steerSpeed);
         transform.Translate(0, moveSpeed, 0);
     }
