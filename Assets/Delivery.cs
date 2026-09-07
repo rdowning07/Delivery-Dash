770d4480b765b0538b4378a4e49ecb75324402e5
robd <redowning@gmail.com>
2026-09-06 23:10:10 -0700

    added bools to package and customer

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -2,16 +2,19 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+   bool hasPackage;
    
    void OnTriggerEnter2D(Collider2D collision)
    {
       if (collision.CompareTag("Package"))
       {
          Debug.Log("Picked up package!");
+         hasPackage = true;
       }
-      if (collision.CompareTag("Customer"))
+      if (collision.CompareTag("Customer") && hasPackage)
       {
          Debug.Log("Delivered package!");
+         hasPackage = false;
       }
    }
 }
